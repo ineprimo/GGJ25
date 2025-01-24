56fb09b5441c57eef782410cb2094332284b2a36
pokoli <psierr01@ucm.es>
2025-01-24 23:05:17 +0100

    Ahora uso el gameManager

--- GGJ25_ProyectoMierda/Assets/Devs/popoli/Scripts/CaquitaMovement.cs
+++ GGJ25_ProyectoMierda/Assets/Devs/popoli/Scripts/CaquitaMovement.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class CaquitaMovement : MonoBehaviour
 {
-    GameObject target;
+    private GameObject player;
     [SerializeField] public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.FindWithTag("Player"); // sorry 
+        player = GameManager.Instance.GetPlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // seguimiento
         float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
-        Vector3 v3 = target.transform.position - transform.position;
-        v3.y = -90.0f;
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+
+        // orientacion
+        Vector3 v3 = player.transform.position - transform.position;
+        v3.y = -90.0f; // para que se mantenga vertical
         transform.rotation = Quaternion.LookRotation(v3);
     }
 }
