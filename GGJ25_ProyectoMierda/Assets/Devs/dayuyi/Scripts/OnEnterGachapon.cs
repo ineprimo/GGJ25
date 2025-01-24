70145bb242c0385abb201952aec2a74e0f11fe1e
(i)nés <ineprimo@ucm.es>
2025-01-25 00:25:45 +0100

    falta un detalle del pulleo
    creo

---
+++ GGJ25_ProyectoMierda/Assets/Devs/dayuyi/Scripts/OnEnterGachapon.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnterGachapon : MonoBehaviour
+{
+    [SerializeField] private bool canPull;
+    [SerializeField] private bool gachaOnCooldown;
+    [SerializeField] private GachaponBase _gacha;
+
+    [SerializeField] private float currentGachaTime = 0;
+
+    private void Start()
+    {
+        canPull = false;
+        gachaOnCooldown = false;
+
+        _gacha.PrepareGacha();
+    }
+    private void Update()
+    {
+        if(canPull)
+        {
+            if (Input.GetKey(KeyCode.E) && !gachaOnCooldown)
+            {
+                // esto va cuando se quiera hacer un pull en el gachapon
+                Upgrade up = _gacha.pull();
+
+                gachaOnCooldown = true;
+                if (up == null)
+                    Debug.Log("no upgrade avaliable");
+                else
+                    Debug.Log(up.getName());
+
+                //
+
+            }
+
+            if (gachaOnCooldown)
+            {
+
+                Debug.Log("gacha on cd");
+                // cooldown
+                if (_gacha.getCD() >= currentGachaTime)
+                {
+                    gachaOnCooldown=false;
+                    currentGachaTime = 0;
+                }
+                currentGachaTime += Time.deltaTime;
+
+                Debug.Log("gacha timer " + currentGachaTime);
+            }
+            
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // si es el player
+        if (other.gameObject.GetComponentInParent<PlayerMovement>() != null)
+        {
+            Debug.Log("entro y soy el player");
+            // le permite tirar en el gachapon
+            canPull = true; 
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // si es el player
+        if (other.gameObject.GetComponentInParent<PlayerMovement>() != null)
+        {
+            // le permite tirar en el gachapon
+            canPull = false;
+        }
+    }
+}
