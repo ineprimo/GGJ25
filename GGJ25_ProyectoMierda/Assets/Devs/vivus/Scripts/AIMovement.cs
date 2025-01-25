6df0afec274461224338b2d55b4b92ced1035eb0
pokoli <psierr01@ucm.es>
2025-01-25 23:06:06 +0100

    un error raro, ya se settea la velocidad del navmesh

--- GGJ25_ProyectoMierda/Assets/Devs/vivus/Scripts/AIMovement.cs
+++ GGJ25_ProyectoMierda/Assets/Devs/vivus/Scripts/AIMovement.cs
@@ -8,29 +8,32 @@ public class AIMovement : MonoBehaviour
     private Transform _playerTransform;
     private NavMeshAgent _agent;
 
-    public void SetSpeed(float sp)
-    {
-        if (_agent != null)
-        {
-            _agent.speed = sp; // Cambia la velocidad de movimiento
-        }
-        else
-        {
-            Debug.LogWarning("NavMeshAgent no está asignado o encontrado.");
-        }
-    }
+
 
     // Start is called before the first frame update
     void Start()
     {
         _playerTransform = GameManager.Instance.GetPlayer().transform;
         _agent = GetComponent<NavMeshAgent>();
+        if (_agent != null) Debug.Log("hay agent");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //_agent.destination = _playerTransform.position;
+        _agent.destination = _playerTransform.position;
         //Debug.Log("Ir");
+    }
+
+    public void SetSpeed(float sp)
+    {
+        if (GetComponent<NavMeshAgent>() != null)
+        {
+            GetComponent<NavMeshAgent>().speed = sp;
+        }
+        else
+        {
+            Debug.LogWarning("NavMeshAgent no está asignado o encontrado.");
+        }
     }
 }
