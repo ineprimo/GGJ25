b226c842dceeb461c919ecc3fc02de9575f21aaf
Jesus Gonzalez <jesugo11@ucm.es>
2025-01-26 11:31:08 +0100

    commit mañanero arreglao

--- GGJ25_ProyectoMierda/Assets/Devs/quexu/Scripts/LeaderboardController.cs
+++ GGJ25_ProyectoMierda/Assets/Devs/quexu/Scripts/LeaderboardController.cs
@@ -3,17 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[System.Serializable]
+public class PlayerScore
+{
+    public string playerName;
+    public int score;
+
+    public PlayerScore(string name, int score)
+    {
+        this.playerName = name;
+        this.score = score;
+    }
+} 
 
 public class LeaderboardController : MonoBehaviour
 {
     public List<TextMeshProUGUI> names;
     public List<TextMeshProUGUI> scores;
-    struct Jugador
+   
+    private const int MAX_ENTRIES = 10;  // Número máximo de entradas en el leaderboard
+    private List<PlayerScore> leaderboard = new List<PlayerScore>();
+
+    // Cargar los datos del leaderboard desde PlayerPrefs
+    void Start()
     {
-        string name; 
-        int score;
+        LoadLeaderboard();
     }
-    
+
+    private void LoadLeaderboard()
+    {
+        leaderboard.Clear(); // Limpiar la lista antes de cargar
+
+        // Cargar los datos de los PlayerPrefs
+        for (int i = 0; i < MAX_ENTRIES; i++)
+        {
+            string playerName = PlayerPrefs.GetString("PlayerName_" + i, "");
+            int score = PlayerPrefs.GetInt("PlayerScore_" + i, 0);
+
+            if (!string.IsNullOrEmpty(playerName))
+            {
+                leaderboard.Add(new PlayerScore(playerName, score));
+            }
+        }
+
+        // Ordenar la lista de mayor a menor por puntuación
+        leaderboard.Sort((x, y) => y.score.CompareTo(x.score));
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
